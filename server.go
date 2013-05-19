c5f7244299526c5668890c9b709277d605374c07
David A. Cuadrado <krawek@gmail.com>
2013-05-19 16:03:31 -0500

    Implemented endpoints needed for git clone.
    now it's possible to clone a repo.

--- server.go
+++ server.go
@@ -8,10 +8,17 @@ import (
     "log"
     "os/exec"
     "io"
+    "io/ioutil"
+    "bytes"
 )
 
-func WriteGitToHttp(w http.ResponseWriter, args ...string) {
-    cmd := exec.Command("git", args...)
+type GitCommand struct {
+    procInput *bytes.Reader
+    args []string
+}
+
+func WriteGitToHttp(w http.ResponseWriter, gitCommand GitCommand) {
+    cmd := exec.Command("git", gitCommand.args...)
     stdout, err := cmd.StdoutPipe()
 
     if err != nil {
@@ -20,13 +27,22 @@ func WriteGitToHttp(w http.ResponseWriter, args ...string) {
         return
     }
 
+    if gitCommand.procInput != nil {
+        cmd.Stdin = gitCommand.procInput
+    }
+
+
     if err := cmd.Start(); err != nil {
         w.WriteHeader(404)
         log.Fatal("Error:", err)
         return
     }
 
-    io.Copy(w, stdout)
+    nbytes, err := io.Copy(w, stdout)
+    log.Printf("Bytes written: %d", nbytes)
+    if err != nil {
+        log.Fatal("Error writing to socket", err)
+    }
 }
 
 
@@ -39,8 +55,25 @@ func getInfoRefs(route *Route, w http.ResponseWriter, r *http.Request) {
     str := "# service=git-upload-pack"
     fmt.Fprintf(w, "%.4x%s\n", len(str)+5, str )
     fmt.Fprintf(w, "0000")
-    WriteGitToHttp(w, "upload-pack", "--stateless-rpc", "--advertise-refs", ".")
+    WriteGitToHttp(w, GitCommand{args: []string{"upload-pack", "--stateless-rpc", "--advertise-refs", "."}} )
 }
+
+func uploadPack(route *Route, w http.ResponseWriter, r *http.Request) {
+    log.Printf("uploadPack for %s", route.RepoPath)
+    // TODO: find repo path at route.RepoPath
+
+    w.Header().Set("Content-Type", "application/x-git-upload-pack-result")
+
+    requestBody, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+        w.WriteHeader(404)
+        log.Fatal("Error:", err)
+        return
+    }
+
+    WriteGitToHttp(w, GitCommand{procInput: bytes.NewReader(requestBody), args: []string{"upload-pack", "--stateless-rpc", "."}})
+}
+
 
 func getRepoFile(route *Route, w http.ResponseWriter, r *http.Request) {
     log.Printf("getRepoFile for %s", route.RepoPath)
@@ -71,6 +104,7 @@ func NewParsedRoute(repoName string, file string, matcher RouteMatcher) *Route {
 
 var Routes = []RouteMatcher{
     RouteMatcher{Matcher: regexp.MustCompile("(.*?)/info/refs$"), Handler: getInfoRefs},
+    RouteMatcher{Matcher: regexp.MustCompile("(.*?)/git-upload-pack$"), Handler: uploadPack},
     RouteMatcher{Matcher: regexp.MustCompile("(.*?)/HEAD$"), Handler: getRepoFile},
 }
 
@@ -96,7 +130,7 @@ func MatchRoute(r *http.Request) *Route {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Printf("Hi there, I love %s!\n", r.URL.Path[0:])
+    log.Printf("Processing %s!\n", r.URL.Path[0:])
 
     parsedRoute := MatchRoute(r)
     if parsedRoute != nil {
